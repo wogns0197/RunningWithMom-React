5554dc537f8966e7a2119c80c5d116186419aff7
wogns0197 <wogns0197@naver.com>
2021-04-20 18:23:03 +0900

    enh/Parse record data to graph

--- src/pages/MainChart.tsx
+++ src/pages/MainChart.tsx
@@ -1,106 +1,32 @@
 import React, { FC } from 'react';
 
 import { Record } from '../types/index';
-import { ResponsiveBar } from '@nivo/bar'
+import { ResponsiveBullet } from '@nivo/bullet'
 import { RootState } from '../store/Store';
 import styled from 'styled-components'
 import { useSelector } from 'react-redux';
 
-const MyResponsiveBar = (data : Record[]) => (
-  <ResponsiveBar
-      data={data}
-      keys={[ 'goal', 'records']}
-      indexBy="day"
-      margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-      padding={0.3}
-      valueScale={{ type: 'linear' }}
-      indexScale={{ type: 'band', round: true }}
-      colors={{ scheme: 'nivo' }}
-      defs={[
-          {
-              id: 'dots',
-              type: 'patternDots',
-              background: 'inherit',
-              color: '#38bcb2',
-              size: 4,
-              padding: 1,
-              stagger: true
-          },
-          {
-              id: 'lines',
-              type: 'patternLines',
-              background: 'inherit',
-              color: '#eed312',
-              rotation: -45,
-              lineWidth: 6,
-              spacing: 10
-          }
-      ]}
-      fill={[
-          {
-              match: {
-                  id: 'fries'
-              },
-              id: 'dots'
-          },
-          {
-              match: {
-                  id: 'sandwich'
-              },
-              id: 'lines'
-          }
-      ]}
-      borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'country',
-          legendPosition: 'middle',
-          legendOffset: 32
-      }}
-      axisLeft={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'food',
-          legendPosition: 'middle',
-          legendOffset: -40
-      }}
-      labelSkipWidth={12}
-      labelSkipHeight={12}
-      labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
-      legends={[
-          {
-              dataFrom: 'keys',
-              anchor: 'bottom-right',
-              direction: 'column',
-              justify: false,
-              translateX: 120,
-              translateY: 0,
-              itemsSpacing: 2,
-              itemWidth: 100,
-              itemHeight: 20,
-              itemDirection: 'left-to-right',
-              itemOpacity: 0.85,
-              symbolSize: 20,
-              effects: [
-                  {
-                      on: 'hover',
-                      style: {
-                          itemOpacity: 1
-                      }
-                  }
-              ]
-          }
-      ]}
-      animate={true}
-      motionStiffness={90}
-      motionDamping={15}
+type CharDataType = {
+  id: string|number,
+  title?: React.ReactNode,
+  ranges: number[],
+  measures: number[],
+  markers?: number[],
+}
+
+const MyResponsiveBullet = (data : CharDataType[]) => (
+  <ResponsiveBullet
+    data={data}
+    margin={{ top: 50, right: 90, bottom: 50, left: 90 }}
+    layout="vertical"
+    spacing={46}
+    titleAlign="start"
+    titleOffsetX={0}
+    titleOffsetY={500}
+    measureSize={0.2}
   />
-)
+);
+
 type Props = {
   key: string,
   year: number,
@@ -125,11 +51,35 @@ const MainChart: FC<Props> = ({
   goal,
   records }) =>
 {
+  const tmp = [
+    {
+      "id": "4월 21일",
+      "ranges": [100],
+      "measures": [
+        33
+      ],
+      "markers": [
+        80
+      ]
+    },
+    
+  ];
+  const parsedData: CharDataType[] = [];
   const storeData = useSelector((state: RootState) => state.reducer);
+  storeData.map(el => {
+    const parsingData = {
+      id: el.month + '/' + el.day,
+      ranges: [100],
+      measures: [el.records],
+      markers: [el.goal],
+    };
+    parsedData.push(parsingData);
+    return parsedData;
+  });
   // console.log(storeData);
   return (
     <Main>
-      {MyResponsiveBar(storeData)}
+      { MyResponsiveBullet(parsedData) }
     </Main>
   );
 };
