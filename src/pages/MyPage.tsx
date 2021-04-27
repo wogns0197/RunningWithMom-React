a7d44d60914c4c3379e665b1761fdd3452b83247
wogns0197 <wogns0197@naver.com>
2021-04-27 23:19:12 +0900

    enh/Check Login

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,8 +1,9 @@
 import { IsMobile, Record } from '../types/index';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import styled, { css } from 'styled-components';
 
 import axios from 'axios';
+import { isNull } from 'node:util';
 import { useMediaQuery } from "react-responsive";
 
 const FLEX = css`
@@ -17,21 +18,20 @@ const Main = styled.div`
   height: 500px;  
 `;
 
-const But = styled.button`
-  width: 100px;
-  height: 50px;
-`;
-
 const Cont = styled.div`  
   width: ${(props: IsMobile) => props.isMobile ? '80%' : '600px'};
-  height: 400px;
+  height: 350px;
   border: 3px solid ${({ theme }) => theme.colors.bisque};
   border-radius: 10px;
+  ${FLEX};
+  justify-content: flex-start;
+  flex-direction: column;
 `;
 
 const InputCont = styled.div`
+  margin-top: 20px;
   width: 100%;
-  height: 300px; 
+  height: 150px; 
   ${FLEX};
   flex-direction: column;
 `
@@ -43,7 +43,7 @@ const Column = styled.div`
   margin-bottom: 20px;
   width: ${(props: IsMobile) => props.isMobile ? '60%' : '40%'};
   height: 30px;
-  border: 2px solid ${({ theme }) => theme.colors.bisque};
+  border: 3px solid ${({ theme }) => theme.colors.bisque};
   border-radius: 10px;
   overflow: hidden;
 `;
@@ -66,10 +66,11 @@ const StyledInput = styled.input`
   color : ${({ theme }) => theme.colors.darkgreen};
   &:focus {
     outline:none;
-  }
+  };
 `
 
-const Header = styled.div` // title 왼쪽으로 치우쳐있음
+// title 왼쪽으로 치우쳐있음 
+const Header = styled.div`
   ${FLEX};
   width: 100%;
   height: 50px;
@@ -84,6 +85,22 @@ const Header = styled.div` // title 왼쪽으로 치우쳐있음
   letter-spacing: 10px;  
 `;
 
+const Button = styled.button`
+  width: 100px;
+  height: 50px;
+  margin-top: 30px;  
+  background-color: ${({ theme }) => theme.colors.pastelgreen};
+  border: 2px solid ${({ theme }) => theme.colors.mediumseagreen};
+  border-radius: 10px;
+  color: ${({ theme }) => theme.colors.darkgreen};
+  font-size: 12pt;
+  cursor: pointer;
+`;
+
+const Invalid = styled.div`
+  color: ${({ theme }) => theme.colors.tomato};
+`;
+
 const getdata = async () => {
   await axios.get('http://localhost:5000/api/getdata')
     .then(res => {
@@ -95,25 +112,49 @@ const getdata = async () => {
     })
 }
 
+
 const MyPage: FC = () => {
-  const isMobile = useMediaQuery({query : "(max-width:767px)",});  
+  const isMobile = useMediaQuery({ query: "(max-width:767px)", });
+  const [id, setID] = useState<string>('');
+  const [pw, setPW] = useState<string>('');
+  const [isValid, setIsValid] = useState<boolean>(false);
+
+  const postLogin = async (id: string, pw: string) => {
+    await axios.post('http://localhost:5000/api/getLogin', { id: id, pw: pw })
+      .then(res => {
+        return res.data.length >= 1 ? true : setIsValid(true);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  
   return (
-    <Main>
-      {/* <But onClick={ async() => {        
-        console.log(await getdata());
-      }}/> */}
+    <Main>      
       <Cont isMobile={isMobile}>
         <Header>LOGIN</Header>
         <InputCont>
           <Column isMobile={isMobile}>
             <InputName>ID</InputName>
-            <StyledInput type='text'></StyledInput>
+            <StyledInput
+              type='text'
+              value={id}
+              onChange={(e) => setID(e.target.value)}
+            />
           </Column>
           <Column isMobile={isMobile}>
             <InputName>PW</InputName>
-            <StyledInput type='password'></StyledInput>
+            <StyledInput
+              type='password'
+              value={pw}
+              onChange={(e) => setPW(e.target.value)}
+            />
           </Column>
         </InputCont>
+        {isValid && (<Invalid>등록되지 않은 ID이거나 잘못된 PW입니다.</Invalid>)}
+        <Button
+          onClick={() => postLogin(id, pw)}>
+          LOGIN</Button>
       </Cont>
     </Main>
   );
